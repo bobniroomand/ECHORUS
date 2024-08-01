3c10127e8dd26f2518e7e3549856b24334ed1e28
Babak Niroomand <bobniroomand@gmail.com>
2024-08-01 16:33:28 +0200

    first endpoint

---
+++ hacker_news_api.py
@@ -0,0 +1,21 @@
+import requests 
+
+
+class HackerNewsAPI:
+    TOP_STORIES_API = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+    ITEM_API = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'
+
+    def get_top_stories():
+        response = requests.get(HackerNewsAPI.TOP_STORIES_API)
+        top_stories = response.json()
+        return top_stories
+    
+    def get_item(item_id):
+        response = requests.get(HackerNewsAPI.ITEM_API.format(item_id))
+        item = response.json()
+        return item
+    
+    def get_comments(item_id):
+        item = HackerNewsAPI.get_item(item_id)
+        kids = item.get('kids', [])
+        return kids
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
+from hacker_news_api import HackerNewsAPI
+
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/endpoint1")
+async def endpoint1():
+    top_100_stories = HackerNewsAPI.get_top_stories()[:100]
+    comments = []
+    for story in top_100_stories:
+        comments.extend(HackerNewsAPI.get_comments(story)[:50])
+    return {"comments": comments}
